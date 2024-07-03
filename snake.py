8f2175d938198c5d8ddf159269f87882e6c103b0
Claudiu357 <neculaclaudiu21@gmail.com>
2024-07-03 12:56:58 +0300

    i made the snake move with the keys from keyboard

--- snake.py
+++ snake.py
@@ -3,19 +3,41 @@ from turtle import Turtle
 
 class Snake:
     STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+    MOVE_DISTANCE = 20
+    UP = 90
+    DOWN = 270
+    LEFT = 180
+    RIGHT = 0
 
     def __init__(self):
         self.segments = []
         for position in self.STARTING_POSITIONS:
-            self.new_segment = Turtle(shape="square")
-            self.new_segment.color("white")
-            self.new_segment.penup()
-            self.new_segment.goto(position)
-            self.segments.append(self.new_segment)
+            new_segment = Turtle(shape="square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+        self.head = self.segments[0]
 
     def move(self):
         for seg_number in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_number - 1].xcor()
             new_y = self.segments[seg_number - 1].ycor()
             self.segments[seg_number].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.segments[0].forward(self.MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != self.DOWN:
+            self.head.setheading(self.UP)
+
+    def left(self):
+        if self.head.heading() != self.RIGHT:
+            self.head.setheading(self.LEFT)
+
+    def down(self):
+        if self.head.heading() != self.UP:
+            self.head.setheading(self.DOWN)
+
+    def right(self):
+        if self.head.heading() != self.LEFT:
+            self.head.setheading(self.RIGHT)
